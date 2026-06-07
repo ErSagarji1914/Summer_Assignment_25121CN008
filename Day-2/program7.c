e541397ae2746af713c0949ae11cbf5b6551c9dd
Sagar Gupta <sagargupta923546@gmail.com>
2026-06-07 21:38:36 +0530

    Day 2 assignment completed

---
+++ Day-2/program7.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int num, digit;
+    int product = 1;
+
+    printf("Enter a number:");
+    scanf("%d",&num);
+
+    while(num!=0){
+        digit = num % 10;
+        product = product * digit;
+        num = num /10;
+
+
+    }
+    printf("Product of digit = %d",product);
+
+    return 0;
+}
