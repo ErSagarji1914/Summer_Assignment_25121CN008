77e3206ca873cf604001766f0580ffb162c3d7bb
Sagar Gupta <sagargupta923546@gmail.com>
2026-06-24 18:06:57 +0530

    Day-17 Assignment Completed

---
+++ Day-17/program66.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main()
+{
+    int arr1[100], arr2[100], unionArr[200];
+    int i, n1, n2, j, k = 0, found;
+
+    printf("Enter the size of first array:");
+    scanf("%d", &n1);
+
+    printf("Enter element of first array:\n");
+
+    for(i = 0; i < n1; i++)
+    {
+        scanf("%d", &arr1[i]);
+        unionArr[k++] = arr1[i];
+    }
+
+    printf("Enter the elements of second array:\n");
+    for(i = 0; i < n2; i++)
+    {
+        scanf("%d", &arr2[i]);
+    }
+
+    for(i = 0; i < n2; i++)
+    {
+        found = 0;
+
+        for(j = 0; j < k; j++)
+        {
+            if(arr2[i] == unionArr[j])
+            {
+                found = 1;
+                break;
+            }
+        }
+        if(found == 0)
+        {
+            unionArr[k++] = arr2[i];
+        }
+    }
+    printf("Union of arrays:\n");
+    for(i = 0; i < k; i++)
+    {
+        printf("%d", unionArr[i]);
+    }
+    return 0;
+
+}
